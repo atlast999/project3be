5645c7fa7199690871b6ce7f6d41c99bf80f172d
hoan.nt <hoan.nguyenthanh@vti.com.vn>
2022-12-29 11:01:13 +0700

    test web server

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hi there")
+	server := gin.Default()
+	server.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(
+			http.StatusOK, gin.H{
+				"message": "pong",
+			})
+	})
+	server.Run()
 }
